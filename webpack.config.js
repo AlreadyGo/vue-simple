40d6742f49d67c27ad386267263ba26a5eb4e63a
zhouhui <18862245093@163.com>
2016-12-24 01:52:48 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,26 @@ module.exports = {
         loaders: ['style', 'css'],
         include: __dirname,
       },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff2"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
     ]
   },
   plugins: [
