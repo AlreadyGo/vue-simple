da38bdb5484f96ab5807ff550a0967d500ae45cb
zhou <huiz@easipay.net>
2016-12-26 17:38:11 +0800

    add alertify

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import components from './components';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../static/css/styles.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min'
+import '../node_modules/alertifyjs/build/css/alertify.min.css'
 
 Object.keys(components).forEach((key) => {
     var name = key.replace(/(\w)/, (v) => v.toUpperCase()) //首字母大写
